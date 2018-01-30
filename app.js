342833dd7eaf9bab52b7d8185896c4ca5ce95ef9
Richardson <Messirichards@gmail.com>
2018-01-30 08:43:14 -0500

    okay again

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const passport = require('passport')
 const flash           = require('connect-flash')
 const session         = require('express-session')
 const app             = express()
-const PORT            = 8080
+const PORT            = process.env.PORT || 8080
 
 // Load Routes
 const ideas = require('./routes/ideas')

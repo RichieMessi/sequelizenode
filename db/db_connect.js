187914edcc3cd02de557612b07a262f0125f5160
Richardson <Messirichards@gmail.com>
2018-01-30 12:39:07 -0500

    bas yaar lge hue hain

--- db/db_connect.js
+++ db/db_connect.js
@@ -3,7 +3,7 @@ const Sequelize       = require('sequelize')
 // SETUP SQQUELIZE
 const connect_to_mysql = new Sequelize('heroku_a43926edad143bbs', 'b3743c2749fc58', '1e437529',{
     host: 'us-cdbr-iron-east-05.cleardb.net',
-    dialect: 'mysql2'
+    dialect: 'mysql'
 }) 
 
 

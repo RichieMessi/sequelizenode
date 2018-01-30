703344e7d86dbc79b5c6a86bc015bf3c9a942dc7
Richardson <Messirichards@gmail.com>
2018-01-30 09:10:51 -0500

    again

--- db/db_connect.js
+++ db/db_connect.js
@@ -1,9 +1,9 @@
 const Sequelize       = require('sequelize')
 
 // SETUP SQQUELIZE
-const connect_to_mysql = new Sequelize('note_ideas', 'b3743c2749fc58', '1e437529',{
+const connect_to_mysql = new Sequelize('heroku_a43926edad143bbs', 'b3743c2749fc58', '1e437529',{
     host: 'us-cdbr-iron-east-05.cleardb.net',
-    dialect: 'heroku_a43926edad143bb'
+    dialect: 'mysql | clearDB'
 })
 
 
